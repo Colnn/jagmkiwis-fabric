684825f63f7599fbb7e8d939abb2e9df64360e6e
Colnn <csoer1947@student.landstede.nl>
2024-02-21 09:01:33 +0100

    Fixed Cat mixin

--- src/main/java/jagm/jagmkiwis/mixins/CatEntityMixin.java
+++ src/main/java/jagm/jagmkiwis/mixins/CatEntityMixin.java
@@ -1,11 +1,15 @@
-package jagm.jagmkiwis;
+package jagm.jagmkiwis.mixins;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import jagm.jagmkiwis.KiwiEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.GoalSelector;
 import net.minecraft.entity.ai.goal.UntamedActiveTargetGoal;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.CatEntity;
 import net.minecraft.entity.passive.RabbitEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,13 +19,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.function.Predicate;
 
 @Mixin(CatEntity.class)
-public class CatEntityMixin {
-    @Shadow
-    GoalSelector targetSelector;
+public class CatEntityMixin extends MobEntity {
+
+    protected CatEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @Inject(method = "initGoals", at = @At("TAIL"))
     protected void addGoals(CallbackInfo ci) {
-        this.targetSelector.add(1, new UntamedActiveTargetGoal<>(cat, RabbitEntity.class, false, null));
-
+        this.targetSelector.add(1, new UntamedActiveTargetGoal<>((CatEntity) (Object)this, KiwiEntity.class, false, null));
     }
 }
